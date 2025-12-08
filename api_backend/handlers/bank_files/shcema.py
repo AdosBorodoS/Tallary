9433380a7101bd5a27dbab4ac47b0794cfe37764
Данила Бородин <danila.borodin@evalar.com>
2025-12-08 18:14:25 +0300

    add bank handlers

---
+++ api_backend/handlers/bank_files/shcema.py
@@ -0,0 +1,26 @@
+import json
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import date
+
+class BaseTools(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    def to_dict(self):
+        return json.loads(self.model_dump_json(by_alias=True, exclude_unset=True))
+
+class AlfaUpdateData(BaseTools):
+    operationDate:date | None = Field(default=None)
+    postingDate:date | None = Field(default=None)
+    code:str | None = Field(default=None)
+    category:str | None = Field(default=None)
+    description:str | None = Field(default=None)
+    currencyAmount:float | None = Field(default=None)
+    status:str | None = Field(default=None)
+
+class TinkoffUpdateData(BaseTools):
+    operationDate:date | None = Field(default=None)
+    postingDate:date | None = Field(default=None)
+    description:str | None = Field(default=None)
+    description2:str | None = Field(default=None)
+    currencyAmount:float | None = Field(default=None)
+    amount:float | None = Field(default=None)
